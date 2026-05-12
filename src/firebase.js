c344c9660049c89812a851fe9819a59d81bdceee
jeromedecomarmond-lgtm <jeromedecomarmond@gmail.com>
2026-05-12 12:38:59 +0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-jsimport { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
